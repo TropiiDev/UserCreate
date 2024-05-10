dee2c249b6db603aa5d99e7a44246130d88909bb
TropiiDev <colinbassett28@icloud.com>
2024-05-10 09:26:42 -0400

    Finished register JS

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,8 +1,11 @@
 const submitBtn = document.querySelector('.submit-button');
 
 submitBtn.addEventListener('click', () => {
-  let inputs = document.querySelectorAll('.user-input');
-  console.log(inputs);
-  localStorage.setItem('email', inputs[0].innerText);
-  alert(localStorage.getItem('email'));
+  const email = document.querySelector('#email').value;
+  const username = document.querySelector('#username').value;
+  const password = document.querySelector('#password').value;
+
+  localStorage.setItem('email', email);
+  localStorage.setItem('username', username);
+  localStorage.setItem('password', password);
 });
