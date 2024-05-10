cfe6823238b43719292e5cfcae6520ca21932db6
TropiiDev <colinbassett28@icloud.com>
2024-05-10 18:49:45 -0400

    Added Account Manager

---
+++ assets/js/account.js
@@ -0,0 +1,8 @@
+const imgController = document.querySelector('#profile-img');
+const savePreferencesBtn = document.querySelector('.save-btn');
+
+savePreferencesBtn.addEventListener(() => {
+  // check if user has an image set, if not hide the image and just show the input
+  // save img to imgLink in localStorage
+  // add change username, email, password functionality
+})
